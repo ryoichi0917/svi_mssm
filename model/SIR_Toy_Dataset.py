27ea2e72d4058cb8b390917f0388e69bbab2b8c8
ryoichi0917 <ryoichi.ishizuka@gmail.com>
2024-04-29 12:14:09 +0000

    update

--- model/SIR_Toy_Dataset.py
+++ model/SIR_Toy_Dataset.py
@@ -30,3 +30,30 @@ class SIR_Toy_Dataset(Dataset):
 
         else:
             return {"I_obs" : I_obs}
+
+
+class SIR_Sample_Dataset(Dataset):
+    #可視化用のsampleデータセットを作成する関数
+    def __init__(self, data_arr):
+        #各クラスタに属する最初のデータを抜き出す
+        tgt_data_list = [0,300]
+        
+        self.I_obs = data_arr["I_obs"][tgt_data_list]
+        self.cluster = data_arr["cluster"][tgt_data_list]
+        self.S = data_arr["S"][tgt_data_list]
+        self.I = data_arr["I"][tgt_data_list]
+        self.R = data_arr["R"][tgt_data_list]
+
+    def __len__(self):
+        return len(self.I_obs)
+
+    def __getitem__(self, idx):
+        #LSTMの入力にするため、特徴量の次元を追加する。
+        I_obs = torch.tensor(self.I_obs[idx], dtype=torch.float32).unsqueeze(1)
+        cluster = torch.tensor(self.cluster[idx], dtype=torch.int32)
+        S = torch.tensor(self.S[idx], dtype=torch.float32)
+        I = torch.tensor(self.I[idx], dtype=torch.float32)
+        R = torch.tensor(self.R[idx], dtype=torch.float32)
+        return {"cluster":cluster, 
+                "S" : S, "I" : I, 
+                "R" : R, "I_obs" : I_obs}
